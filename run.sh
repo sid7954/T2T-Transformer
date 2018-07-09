69f19bc98408ec6c7ecb161108e874ec6d58e680
Siddhant Garg <sid7954@gmail.com>
2018-07-09 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/multistep_optimizer.py to T2T Transformer

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tmp/*.py; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to T2T Transformer"
-    GIT_COMMITTER_DATE="Wed 11 Jul 2018 20:19:19 BST" git commit --amend --no-edit --date "Wed 11 Jul 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Mon 9 Jul 2018 20:19:19 BST" git commit --amend --no-edit --date "Mon 9 Jul 2018 20:19:19 BST"
 done
 git push origin master
