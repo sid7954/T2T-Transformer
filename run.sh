0cfe93faeff45f179b28ba40cef2c795d74a8a4b
Siddhant Garg <sid7954@gmail.com>
2018-08-08 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/transformer_revnet.py to T2T Transformer

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tmp/*.py; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to T2T Transformer"
-    GIT_COMMITTER_DATE="Tue 7 Aug 2018 20:19:19 BST" git commit --amend --no-edit --date "Tue 7 Aug 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Wed 8 Aug 2018 20:19:19 BST" git commit --amend --no-edit --date "Wed 8 Aug 2018 20:19:19 BST"
 done
 git push origin master
