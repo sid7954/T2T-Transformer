d3d9ca27bb29f9f49d8b3e61d6819554d9de9d05
Siddhant Garg <sid7954@gmail.com>
2018-09-27 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/trainer_model_based_agent_only.py to T2T Transformer

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tmp/*.py; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to T2T Transformer"
-    GIT_COMMITTER_DATE="Sun 26 Aug 2018 20:19:19 BST" git commit --amend --no-edit --date "Sun 26 Aug 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Thu 27 Sep 2018 20:19:19 BST" git commit --amend --no-edit --date "Thu 27 Sep 2018 20:19:19 BST"
 done
 git push origin master
